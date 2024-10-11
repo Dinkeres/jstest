4d47877f8203a4759eef3f4f6b2ce3fb9017361e
Dinkeres <121654391+Dinkeres@users.noreply.github.com>
2024-10-11 11:19:54 -0400

    Update and rename index.html to emulator.js

---
+++ emulator.js
@@ -0,0 +1,11 @@
+EJS_player = "#game";
+EJS_core = "gba";
+EJS_gameName = "fire red";
+EJS_color = "#0064ff";
+EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
+EJS_gameUrl = "https://a06aa4a1-6cc5-499d-b5f9-1c31087b7733.usrfiles.com/ugd/a06aa4_e5843a92e956499cbb21374422d2d600.json";
+
+// Load the loader.js script
+const script = document.createElement('script');
+script.src = 'https://cdn.emulatorjs.org/stable/data/loader.js';
+document.body.appendChild(script);
